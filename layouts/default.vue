394971120d91a6321ea9ff11d86635805699b940
Ev777gen <fortheartist@mail.ru>
2023-04-11 11:08:53 +0500

    add homepage

---
+++ layouts/default.vue
@@ -0,0 +1,113 @@
+<template>
+  <!--<div class="app" :style="isDarkMode ? { backgroundColor: '#5f5f65', color: '#fff' } : null">-->
+  <div class="app">
+    <header class="app__header">
+      <TheNavbar/>
+    </header>
+    <hr>
+    <div class="app__body container">
+      <!--<aside class="app__sidebar desktop-only" :style="isDarkMode ? { backgroundColor: '#555' } : null">-->
+      <aside class="app__sidebar desktop-only">
+        <TheSidebar />
+      </aside>
+      <main class="app__content">
+        <!-- <TheBreadcrumbs />-->
+        <slot />
+        <!-- <router-view v-show="isAsyncDataLoaded" :key="`${$route.path}${JSON.stringify($route.query)}`" /> -->
+        <!--<AppSpinner v-show="!isAsyncDataLoaded" class="app__spinner" />-->
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+/*import TheBreadcrumbs from '@/components/layout/TheBreadcrumbs';
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from '@/stores/AuthStore';
+import { useForumStore } from '@/stores/ForumStore';
+import router from "@/router";
+import NProgress from 'nprogress';
+import useDarkMode from '@/composables/useDarkMode';
+
+const { isAsyncDataLoaded } = storeToRefs(useForumStore());
+const { fetchAuthUser } = useAuthStore();
+const { isDarkMode } = useDarkMode();
+
+fetchAsyncData();
+
+async function fetchAsyncData() {
+  await fetchAuthUser();
+  NProgress.configure({
+    speed: 200,
+    showSpinner: false
+  });
+  router.beforeEach(() => {
+    NProgress.start();
+  });
+  router.afterEach(() => {
+    setTimeout(() => NProgress.done(), 500);
+  });
+}*/
+</script>
+
+<style lang="scss">
+@import '@/assets/reset.css';
+@import '@/assets/global.scss';
+/*@import "~nprogress/nprogress.css";*/
+
+$container-width: 1100px;
+$content-width: 900px;
+$header-height: 75px;
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  max-width: $container-width;
+  margin: 0px auto;
+  padding: 0 15px;
+}
+
+.app {
+  min-height: 100vh;
+  background-color: #fff;
+  
+  &__header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: $header-height;
+    background-color: #23374d;
+    z-index: 999;
+  }
+  
+  &__body {
+    margin-top: $header-height;
+  }
+
+  &__content {
+    width: $content-width;
+    margin-left: $container-width - $content-width + 15px;
+    @media (max-width: 720px) {
+      & {
+        margin-left: 0px;
+      }
+    }
+  }
+
+  &__sidebar {
+    position: fixed;
+    width: $container-width - $content-width;
+    min-height: 100%;
+    padding: 25px 15px;
+    background-color: #f6f6f6;
+  }
+
+  &__spinner {
+    margin-top: 40vh;
+  }
+}
+/*
+#nprogress .bar{
+  background: #5ce0b0 !important;
+}*/
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="homepage">
+    <h1 class="homepage__title title">Single Page Application</h1>
+    <h2 class="homepage__subtitle subtitle">Форум на Vue 3 и Firebase</h2>
+    <div class="homepage__text">
+      <p>
+        Привет! Это пример реализации SPA в виде 
+        <!--<router-link :to="{name: 'ForumMainPage'}" class="homepage__link">форума</router-link>.-->
+        Здесь можно зарегистрироваться, создать новую тему, написать пост, 
+        отредактировать свои данные на странице профиля и поменять e-mail в настроках. 
+        Функционал и дизайн специально упрощены, чтобы было нагляднее.
+      </p>
+      <p>А еще приложение адаптировано под мобильные устройства.</p>
+    </div>
+    <div class="homepage__text">
+      Для backend-части проекта я использовал Firebase 9: 
+      <ol  class="homepage__list">
+        <li>Cloud Firestore - в качестве базы данных;</li>
+        <li>Authentication - для авторизации;</li>
+        <li>Storage - для хранения аватарок, которые загружает пользователь.</li>
+      </ol>
+    </div>
+    <div class="homepage__text">
+      На GitHub можно посмотреть разные варианты исходного кода:
+      <ol class="homepage__list">
+        <li><a href="https://github.com/Ev777gen/SPA" target="_blank">Исходный код</a> (Vue.js 3 Optios API + Vuex);</li>
+        <li><a href="https://github.com/Ev777gen/SPA-forum-using-Pinia" target="_blank">Исходный код</a> (Vue.js 3 Composition API + Pinia).</li>
+      </ol>
+    </div>
+    <div class="homepage__text">
+      Чтобы осмотреться вокруг, можно 
+      <!--<router-link :to="{name: 'RegisterForm'}" class="homepage__link">
+        зарегистрироваться
+      </router-link>-->
+      самостоятельно, используя любую почту и пароль, или 
+      <!--<button class="btn btn_green" @click.prevent="logInToCheckOutThisSite" :disabled="authUser">
+        войти в аккаунт существующего пользователя
+      </button>-->
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+/*import { storeToRefs } from 'pinia';
+import { useAuthStore } from '@/stores/AuthStore';
+import { useForumStore } from '@/stores/ForumStore';
+
+const { authUser } = storeToRefs(useAuthStore());
+const { signInWithEmailAndPassword } = useAuthStore();
+const { fetchUser, startLoadingIndicator, stopLoadingIndicator } = useForumStore();
+
+async function logInToCheckOutThisSite() {
+  try {
+    startLoadingIndicator();
+    const defaultUser = await fetchUser({ id: '8WGcARP4RqQchFNE2wh326iwQ913' });
+    await signInWithEmailAndPassword({ email: defaultUser.email, password: '123456' });
+    stopLoadingIndicator();
+  } catch (error) {
+    alert(error.message);
+  }
+}*/
+</script>
+
+<style lang="scss" scoped>
+.homepage {
+  font-size: 16px;
+  line-height: 1.5;
+
+  &__title {
+    margin: 30px 0;
+  }
+  
+  &__subtitle {
+    margin: 20px 0;
+  }
+
+  &__text {
+    margin: 20px 0;
+    text-align: justify;
+    & p {
+      margin: 20px 0;
+    }
+    & button {
+      margin: 15px 0;
+      text-align: center;
+    }
+  }
+
+  &__list {
+    & li {
+      margin-left: 45px;
+    }
+  }
+}
+</style>
